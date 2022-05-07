be9b21da9862af28e1075e5b31af7936c0ebb36a
HUI <luxuhui@agora.io>
2022-05-07 13:07:15 +0800

    optimize KeyCenter

--- Android/app/src/main/java/io/agora/metachat/example/KeyCenter.java
+++ Android/app/src/main/java/io/agora/metachat/example/KeyCenter.java
@@ -5,11 +5,11 @@ public class KeyCenter {
     public static final String CHANNEL_ID = "YourChannelId";
 
     public static final String RTC_APP_ID = "YourAppId";
-    public static final String RTC_TOKEN = "YourToken";
     public static final int RTC_UID = 0; // 不要设置成0！！！
+    public static final String RTC_TOKEN = "YourToken";
 
     public static final String RTM_APP_ID = "YourAppId";
+    public static final String RTM_UID = String.valueOf(RTC_UID); // Demo侧使用String的RTC_UID
     public static final String RTM_TOKEN = "YourToken";
-    public static final String RTM_UID = String.valueOf(RTC_UID);
 
 }
