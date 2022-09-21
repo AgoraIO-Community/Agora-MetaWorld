9305806fff201dc6ed9e63876e6c2e5b0ce30657
Yan Zhennan <yanzhennan@agora.io>
2022-09-21 20:30:25 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,16 @@
+## Get started
+
+### Step
+
+1. Download the SDK from [](), then copy the `*.jar` and `*.aar` to [android/app/libs](app/libs)
+   directory, copy the `*.so` to [android/src/main/jniLibs/${ANDROID_ABI}](app/src/main/jniLibs)
+
+2. Update the configuration which in local.properties.For example:
+```mk
+#app id
+APP_ID=xxxxx
+#app certificate
+APP_CERTIFICATE=xxxxx
+```
+
+3.Run the example
---
+++ app/src/main/java/io/agora/metachat/example/KeyCenter.java
@@ -0,0 +1,40 @@
+package io.agora.metachat.example;
+
+import java.util.Random;
+
+import io.agora.media.RtcTokenBuilder;
+import io.agora.rtm.RtmTokenBuilder;
+
+public class KeyCenter {
+
+    public static final String CHANNEL_ID = "MetaChatTest";
+
+    public static final String APP_ID = BuildConfig.APP_ID;
+    public static final int RTC_UID = new Random().nextInt(1000); // 不要设置成0！！！
+    public static final String RTC_TOKEN = new RtcTokenBuilder().buildTokenWithUid(
+            APP_ID,
+            BuildConfig.APP_CERTIFICATE,
+            CHANNEL_ID,
+            RTC_UID,
+            RtcTokenBuilder.Role.Role_Publisher,
+            0
+    );
+
+    public static final String RTM_UID = String.valueOf(RTC_UID); // Demo侧使用String的RTC_UID
+    public static String RTM_TOKEN = null;
+
+    static {
+        try {
+            RTM_TOKEN = new RtmTokenBuilder().buildToken(
+                    APP_ID,
+                    BuildConfig.APP_CERTIFICATE,
+                    String.valueOf(RTC_UID),
+                    RtmTokenBuilder.Role.Rtm_User,
+                    0
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
